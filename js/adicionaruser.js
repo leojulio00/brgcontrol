94f96dd635c500b207771de22bb170674e31b813
leojulio00 <leonildojuliojulio@gmail.com>
2022-11-29 03:25:30 +0200

    adicao do backend nas paginas login e dashboard

---
+++ js/adicionaruser.js
@@ -0,0 +1,80 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+var codColab = document.querySelector(".codColab")
+var nomeColab = document.querySelector(".nomeColab")
+var enderColab = document.querySelector(".enderColab")
+var telColab = document.querySelector(".telColab")
+var emailColab = document.querySelector(".emailColab")
+var catgColab = document.querySelector(".catgColab")
+var passColab = document.querySelector(".passColab")
+var btnCadasColab = document.querySelector(".btnCadasColab")
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJol3W4SFmIApBs313nhotjjC5Tf4rYFI",
+  authDomain: "bar-rest-14387.firebaseapp.com",
+  databaseURL: "https://bar-rest-14387-default-rtdb.firebaseio.com",
+  projectId: "bar-rest-14387",
+  storageBucket: "bar-rest-14387.appspot.com",
+  messagingSenderId: "278297185259",
+  appId: "1:278297185259:web:f3b96b35c4220f2b18e7ae",
+  measurementId: "G-7P30PF86QN"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+//var db = firebase.database()
+
+
+
+
+
+btnCadasColab.addEventListener("click", ()=>{
+function writeUserData(codColab, nomeColab, enderColab, telColab, emailColab, catgColab) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + codColab), {
+    nomeColab: nomeColab,
+    enderColab: enderColab,
+    telColab: telColab,
+    emailColab: emailColab,
+    catgColab: catgColab
+  });
+}
+
+
+    createUserWithEmailAndPassword(auth, emailColab.value, passColab.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+
+    writeUserData(codColab.value, nomeColab.value, enderColab.value, telColab.value, emailColab.value, catgColab.value)
+
+    alert("usuario criado com sucesso")
+      codColab.innerHTML = ""
+      nomeColab.innerHTML = ""
+      enderColab.innerHTML = ""
+      telColab.innerHTML = ""
+      emailColab.innerHTML = ""
+      catgColab.innerHTML = ""
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("erro: " + errorMessage)
+  });
+
+  /*createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });*/
+})
--- js/teste.js
+++ js/teste.js
@@ -1,9 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
 var inputEmail = document.querySelector(".inputEmail")
 var inputPass = document.querySelector(".inputPass")
 var btnEntrar = document.querySelector(".btnEntrar")
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJol3W4SFmIApBs313nhotjjC5Tf4rYFI",
+  authDomain: "bar-rest-14387.firebaseapp.com",
+  databaseURL: "https://bar-rest-14387-default-rtdb.firebaseio.com",
+  projectId: "bar-rest-14387",
+  storageBucket: "bar-rest-14387.appspot.com",
+  messagingSenderId: "278297185259",
+  appId: "1:278297185259:web:f3b96b35c4220f2b18e7ae",
+  measurementId: "G-7P30PF86QN"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+
+
+
 btnEntrar.addEventListener("click", ()=>{
-    if(inputEmail.value == "leonildojuliojulio@gmail.com" && inputPass.value == "123456"){
-        window.location.href = "dashboard/index.html"
-      }
+  signInWithEmailAndPassword(auth, inputEmail.value, inputPass.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert("usuario criado com sucesso")
+    window.location.href = "dashboard/index.html"
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("erro: " + errorMessage)
+  });
 })
