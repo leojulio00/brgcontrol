47e85eb9be3b07e5d68cf6cec11434a9abb095b7
leojulio00 <leonildojuliojulio@gmail.com>
2022-12-24 00:11:34 +0200

    mudanca da logo e nome para BR Gest, resolucao do bug no grafico desempenho

--- js/chartDesempenho.js
+++ js/chartDesempenho.js
@@ -6,10 +6,10 @@ new Chart(ctx, {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 19, 3, 5, 10, 3],
         borderWidth: 1,
-        borderColor: '#1A227E',
-        backgroundColor: '#1A227E'
+        borderColor: '#423DCC',
+        backgroundColor: '#423DCC'
       }]
     },
     options: {
