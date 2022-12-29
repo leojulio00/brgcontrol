35ca1d3b0dfa0eaf3900d90214e3ef837118d828
leojulio00 <leonildojuliojulio@gmail.com>
2022-12-30 00:02:01 +0200

    finalizacao da funcao alterar senha no modulo usuario

---
+++ js/alterarSenha.js
@@ -0,0 +1,59 @@
+import { firebaseConfig} from "./firebaseConfig.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, updatePassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+var actualPass = document.querySelector(".senhaActual")
+var novaPass = document.querySelector(".novaSenha")
+var confirPass = document.querySelector(".confSenha")
+var btnAlterPass = document.querySelector(".btnAlterarSenha")
+var userLocalStorage = window.localStorage.getItem("user");
+var usuarioMail = ""
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getDatabase(app);
+
+btnAlterPass.addEventListener("click", ()=>{
+  const starCountRef = ref(db, 'users/' + userLocalStorage);
+
+  if(actualPass.value != "" & novaPass.value != "" & confirPass.value != ""){
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      //updateStarCount(postElement, data);
+      usuarioMail = data.emailColab
+  
+      signInWithEmailAndPassword(auth, usuarioMail, actualPass.value).then((userCredential) => {
+        // Signed in 
+        const userA = userCredential.user;
+        // ...
+
+
+        const user = auth.currentUser;
+        /*const newPassword = getASecureRandomPassword();*/
+
+        if(novaPass.value == confirPass.value){
+          updatePassword(user, novaPass.value).then(() => {
+            // Update successful.
+            alert("Senha alterada com sucesso!")
+          }).catch((error) => {
+            // An error ocurred
+            // ...
+            alert("err:  2" + error)
+          });
+        }else{
+          //alert("")
+        }
+
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert("erro:   1" + errorMessage)
+      });
+    });
+  }else{
+    alert("Preencha todos os campos por favor")
+  }
+
+})
